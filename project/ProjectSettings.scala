3cd7b55ba4d492c4932461a98699efd8faf85989
suankidev <suankisingh@outlook.com>
2024-02-20 22:03:20 +0530

    working repo

--- project/ProjectSettings.scala
+++ project/ProjectSettings.scala
@@ -15,7 +15,7 @@ object ProjectSettings {
     organizationName := "suankiDev",
     developers       := List(Developer("Sujeet", "sujeet Singh", "test@mail.com", new URL("http://dev.com"))),
     // scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-Xfuture","-Xlint"),
-    javaOptions ++= Seq("-Xms512M", "-Xmx2048M", "-XX:MaxPermSize=2048M", "-XX:+CMSClassUnloadingEnabled"),
+//    javaOptions ++= Seq("-Xms512M", "-Xmx2048M", "-XX:MaxPermSize=2048M", "-XX:+CMSClassUnloadingEnabled"),
     cancelable in Global := true // allow to use Ctrl + C in sbt prompt
   )
 //  private lazy val commonSettings = general ++ Testing.settings ++ Publish.settings ++ Keys.settings ++ Assembly.settings
