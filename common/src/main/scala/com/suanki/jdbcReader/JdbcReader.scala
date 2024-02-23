7b4f37dcfd439ceddf8beaeef2be152d9bb1d158
suankidev <suankisingh@outlook.com>
2024-02-23 09:27:16 +0530

    sparkdev

---
+++ common/src/main/scala/com/suanki/jdbcReader/JdbcReader.scala
@@ -0,0 +1,39 @@
+package com.suanki.jdbcReader
+
+import java.sql.{Connection, DriverManager, ResultSet, Statement}
+
+object JdbcReader {
+
+  private[this] val DB_URL = "jdbc:oracle:thin:@//localhost:1521/PDBORCL"
+  private[this] val USER   = "suanki"
+  private[this] val PASS   = "TestPass#123"
+  private[this] val QUERY  = ""
+
+  def getJdbcConnection: () => Connection = { () =>
+    {
+      val driver = new oracle.jdbc.driver.OracleDriver()
+      DriverManager.registerDriver(driver)
+
+      val connection: Connection = DriverManager.getConnection(
+        "jdbc:oracle:thin:suanki/TestPass#123@localhost:1521/PDBORCL"
+      )
+      connection
+    }
+  }
+
+  def executeJdbcQuery(query: String): (ResultSet, Connection) = {
+
+    val QUERY                  = query
+    val connection: Connection = getJdbcConnection()
+    val statement: Statement   = connection.createStatement()
+    val resultSet: ResultSet   = statement.executeQuery(QUERY)
+
+    (resultSet, connection)
+
+  }
+
+  def apply(q: String): (ResultSet, Connection) = {
+    executeJdbcQuery(q)
+  }
+
+}
